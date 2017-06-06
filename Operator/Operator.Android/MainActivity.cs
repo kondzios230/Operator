3810dc5dcbe28cea67defc1c2f7ab3ba4b575d47
kondzios230 <kondzios230@gmail.com>
2017-06-06 19:39:53 +0200

    FFimage added

--- Operator/Operator.Android/MainActivity.cs
+++ Operator/Operator.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Droid;
 
 namespace Operator.Droid
 {
@@ -18,7 +19,7 @@ namespace Operator.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-
+            CachedImageRenderer.Init();
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
